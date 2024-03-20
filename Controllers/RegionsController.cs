41c4573312264312f91ec34df86f4afa51c092fb
Jakub <bushmeen@gmail.com>
2024-03-20 22:04:29 +0100

    add regions contorller and and regions DTO

---
+++ Controllers/RegionsController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UdemyCourse.API.Data;
+using UdemyCourse.API.Models.Domain;
+using UdemyCourse.API.Models.DTO;
+
+namespace UdemyCourse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly UdemyCourseDbContext dbContext;
+
+        public RegionsController(UdemyCourseDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+
+            //get data from db - Domain models
+            var regions = dbContext.Regions.ToList();
+
+            // map doamin models to DTOs
+            var regionsDto = new List<RegionDto>();
+            foreach (var region in regions)
+            {
+                regionsDto.Add(new RegionDto() { 
+                    Id = region.Id, 
+                    Code = region.Code,
+                    Name = region.Name, 
+                    RegionImageUrl = region.RegionImageUrl
+                });
+            }
+
+
+            //return DTOs not Domain model
+            return Ok(regionsDto);
+        }
+
+        [HttpGet]
+        //take id from route e.g /api/regions/{id}
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+
+            //CAN ONLY BE USED WITH PRIMARY KEY
+            //var region=dbContext.Regions.Find(id);
+            var region = dbContext.Regions.FirstOrDefault(r => r.Id == id);
+
+            if (region == null)
+                return NotFound();
+
+            RegionDto regionDto = new ()
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name= region.Name,
+                RegionImageUrl = region.RegionImageUrl
+            };
+
+            return Ok(regionDto);
+        }
+    }
+}
---
+++ Models/DTO/RegionDto.cs
@@ -0,0 +1,11 @@
+﻿namespace UdemyCourse.API.Models.DTO
+{
+    public class RegionDto
+    {
+        public Guid Id {get; set; }
+        public required string Name { get; set; }
+        public required string Code { get; set; }
+        public string? RegionImageUrl { get; set; }
+       
+    }
+}
